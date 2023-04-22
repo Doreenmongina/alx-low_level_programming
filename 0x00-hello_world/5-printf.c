05894510ce0f01f599ff3edb1ce25edaca514355
Doreenmongina <dormong0@gmail.com>
2023-04-22 08:25:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - hello fifth task
+ * main - first task for printf function
  *
  * Return: Always 0 (success)
 */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art,/")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0)
 }
