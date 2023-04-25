c81331195d3f7e1bc9ffd60e81c96d14962eba54
Doreenmongina <dormong0@gmail.com>
2023-04-25 13:13:01 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
+
 /**
-* main - program compilation begins from main
-* Description: Program to print all possible combinations of numbers with no
-* two numbers same in pairing
-* Return: 0 (success)
-*/
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
 int main(void)
 {
-	int a, b;
-
-	for (a = 48; a <= 57; a++)
+	int n, m;
+for (n = 48; n <= 56; n++)
 	{
-		for (b = 49; b <= 57; b++)
+		for (m = 49; m <= 57; m++)
 		{
-			if (a != b && b > a)
+			if (m > n)
 			{
-			putchar(a);
-			putchar(b);
-			putchar(',');
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
+
