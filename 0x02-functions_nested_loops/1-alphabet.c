a233b3e51da919a5022e227d3b997b906b65cf1a
Doreenmongina <dormong0@gmail.com>
2023-04-26 13:04:23 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,13 +9,9 @@
 void print_alphabet(void)
 
 {
+	char letter;
 
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
+		for (letter = "a"; letter <= "z"; letter++)
+		_putchar(letter);
 	_putchar('\n');
 }
